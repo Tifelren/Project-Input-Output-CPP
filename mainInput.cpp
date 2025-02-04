ffa927ced51bab67a599f3e501e72f90df46f478
Tiara <tiarafelicia15@gmail.com>
2025-02-04 16:06:07 +0700

    Add files via upload

---
+++ mainInput.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nama;
+    string nis;
+    string nisn;
+    string ttl;
+    string agama;
+    string status;
+    string anak;
+    string alamat;
+    string smp;
+    string no;
+    string ayah;
+    string ibu;
+    string kayah;
+    string kibu;
+
+    cout << "Masukkan Nama : ";
+    getline(cin,nama);
+    cout << "Masukkan NIS : ";
+    getline(cin,nis);
+    cout << "Masukkan NISN : ";
+    getline(cin,nisn);
+    cout << "Masukkan Tempat Tanggal Lahir : ";
+    getline(cin,ttl);
+    cout << "Masukkan Agama : ";
+    getline(cin,agama);
+    cout << "Masukkan Status dalam keluarga : ";
+    getline(cin,status);
+    cout << "Anak ke : ";
+    getline(cin,anak);
+    cout << "Masukkan Alamat : ";
+    getline(cin,alamat);
+    cout << "Masukkan Asal SMP : ";
+    getline(cin,smp);
+    cout << "Masukkan No Telepon : ";
+    getline(cin,no);
+    cout << "Masukkan Nama Ayah : ";
+    getline(cin,ayah);
+    cout << "Masukkan Nama Ibu : ";
+    getline(cin,ibu);
+    cout << "Masukkan Pekerjaan Ayah : ";
+    getline(cin,kayah);
+    cout << "Masukkan Pekerjaan Ibu : ";
+    getline(cin,kibu);
+
+    cout<<endl;
+
+    cout << "===BIODATA RAPORT===" << endl;
+
+    cout<<endl;
+
+    cout << "Nama : " << nama << endl;
+    cout << "NIS : " << nis << endl;
+    cout << "NISN : " << nisn << endl;
+    cout << "Tempat Tanggal Lahir : " << ttl << endl;
+    cout << "Agama : " << agama << endl;
+    cout << "Status dalam keluarga : " << status << endl;
+    cout << "Anak ke : " << anak << endl;
+    cout << "Alamat : " << alamat << endl;
+    cout << "Asal SMP : " << smp << endl;
+    cout << "No Telepon : " << no << endl;
+    cout << "Nama Ayah : " << ayah << endl;
+    cout << "Nama Ibu : " << ibu << endl;
+    cout << "Pekerjaan Ayah : " << kayah << endl;
+    cout << "Pekerjaan Ibu : " << kibu << endl;
+
+    cout<<endl;
+
+    cout << "=====================" << endl;
+
+    cout<<endl;
+}
---
+++ mainLangsung.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nama = "Tiara Felicia Renata";
+    int nis = 24334;
+    string nisn = "0098587039";
+    string ttl = "Semarang, 15 Mei 2009";
+    string agama = "Kristen";
+    string status = "Anak";
+    int anak = 1;
+    string alamat = "Jl. Sriwidodo Utara VII";
+    string smp = "SMP Negeri 16 Semarang";
+    string kota = " Semarang";
+    string no = "08977936799";
+    string ayah = "Fendi";
+    string ibu = "Eliana";
+    string kayah = "Swasta";
+    string kibu = "Swasta";
+
+    cout << "Nama : " << nama << endl;
+    cout << "NIS : " << nis << endl;
+    cout << "NISN : " << nisn << endl;
+    cout << "Tempat Tanggal Lahir : " << ttl << endl;
+    cout << "Agama : " << agama << endl;
+    cout << "Status dalam keluarga : " << status << endl;
+    cout << "Anak ke : " << anak << endl;
+    cout << "Alamat : " << alamat << endl;
+    cout << "Asal SMP : " << smp << endl;
+    cout << "No Telepon : " << no << endl;
+    cout << "Nama Ayah : " << ayah << endl;
+    cout << "Nama Ibu : " << ibu << endl;
+    cout << "Pekerjaan Ayah : " << kayah << endl;
+    cout << "Pekerjaan Ibu : " << kibu << endl;
+}
